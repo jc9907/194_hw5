84aca99c72ebca7df92815267b2286e989567276
jc9907 <jiachen@ucsb.edu>
2020-11-18 23:42:45 -0800

    first

---
+++ Assets/timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    public Text timerText;
+    private float startTime;
+    private bool f = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (f) return;
+        else
+        {
+            float t = Time.time - startTime;
+            string minutes = ((int)t / 60).ToString();
+            string seconds = (t % 60).ToString("f2");
+
+            timerText.text = minutes + ":" + seconds;
+        }
+      
+  
+    }
+    public void finish()
+    {
+        f = true;
+        timerText.color = Color.yellow;
+    }
+}
